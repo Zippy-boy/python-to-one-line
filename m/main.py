291a3d05176e467576e58ca1aebb4837a00e53d2
Zippy-boy <zippy-boy@outlook.com>
2022-02-28 11:35:40 +0000

    good - lapt
    Full main script

--- m/main.py
+++ m/main.py
@@ -1,10 +1,10 @@
 
 
 
-raw_f = open("test.txt",'r',encoding = 'utf-8')
+raw_f = open("m/test.txt",'r',encoding = 'utf-8')
 f = str(raw_f.read())
 
-f = f.strip().replace('\"', '\\"').replace("\'", "\\'").split("\n")
+f = f.strip().replace("    ", "\\t").replace("\\", "\\\\").replace('\"', '\\"').replace("\'", "\\'").split("\n")
 a = []
 for i in f:
     a.append(str(i) + "\\n")
@@ -15,5 +15,5 @@ for i in a:
 
 
 print(n)
-with open('output.py', 'w') as f:
+with open('m/output.py', 'w') as f:
     f.write(f"exec(\"{n}\")")
