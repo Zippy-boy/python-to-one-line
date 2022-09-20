20c90bf6aeba029434311fa807aa514e8e185589
Zippy-boy <zippy-boy@outlook.com>
2022-09-20 12:16:29 +0100

    a
    a

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
 import argparse
-from pathlib import Path
+
 
 def format_box(title, body, width=80):
     box_line = lambda text: "*  " + text + (" " * (width - 6 - len(text))) + "  *"
@@ -16,21 +15,32 @@ def format_box(title, body, width=80):
     print(box_line(""))
     print("*" * width)
 
+
 parser = argparse.ArgumentParser()
 
-parser.add_argument('first', type=str, help="Full path to file")
-parser.add_argument('-o', '--output', type=str, help="The outputed file", required=False)
+parser.add_argument("first", type=str, help="Full path to file")
+parser.add_argument(
+    "-o", "--output", type=str, help="The outputed file", required=False
+)
 
 args = parser.parse_args()
-raw_f = open(args.first,'r',encoding = 'utf-8')
+raw_f = open(args.first, "r", encoding="utf-8")
 f = str(raw_f.read())
-f = f.strip().replace("\t", "\\t").replace("\\", "\\\\").replace('\"', '\\"').replace("\'", "\\'").split("\n")
+f = (
+    f.strip()
+    .replace("\t", "\\t")
+    .replace("\\", "\\\\")
+    .replace('"', '\\"')
+    .replace("'", "\\'")
+    .replace("    ", "\\t")
+    .split("\n")
+)
 a = []
 for i in f:
     a.append(str(i) + "\\n")
 n = ""
 for i in a:
-    n = n+i
+    n = n + i
 
 input_path = args.first
 output_path = args.output
@@ -40,25 +50,20 @@ o = output_path_none.split("\\")
 o.pop()
 output_path_none = ""
 for i in o:
-    output_path_none = output_path_none+i+"\\"
+    output_path_none = output_path_none + i + "\\"
 
 if output_path == None:
     print(output_path_none)
-    with open(output_path_none + "output.py", 'w') as f:
-            f.write(f"exec(\"{n}\")")
-            format_box(
-                "\tOne -- Line",
-
-                "\tOutPut File --->\n\n"
-                f"{output_path_none}\\output.py"
-            );
+    with open(output_path_none + "output.py", "w") as f:
+        f.write(f'exec("{n}")')
+        format_box(
+            "\tOne -- Line", "\tOutPut File --->\n\n" f"{output_path_none}\\output.py"
+        )
 
 else:
-    with open(f"{output_path}\\output.py", 'w') as f:
-        f.write(f"exec(\"{n}\")")
+    with open(f"{output_path}\\output.py", "w") as f:
+        f.write(f'exec("{n}")')
         format_box(
-            "\tOne -- Line",
+            "\tOne -- Line", "\tOutPut File --->\n\n" f"{output_path}\\output.py"
+        )
 
-            "\tOutPut File --->\n\n"
-            f"{output_path}\\output.py"
-        );
